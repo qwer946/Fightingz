1509ac46cfec7609f394f5156e1554d56a8816ea
qwer946 <khalid.ali.987@gmail.com>
2022-05-15 01:26:45 -0500

    wip

--- script.js
+++ script.js
@@ -6,19 +6,26 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
   }
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
   }
 
   update() {
     this.draw();
+
     this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
   }
 }
 
@@ -29,7 +36,7 @@ const player = new Sprite({
   },
   velocity: {
     x: 0,
-    y: 0,
+    y: 10,
   },
 });
 
