3eecbf0b4b5b0f74b3d6da4cd29332b5e38481fb
qwer946 <khalid.ali.987@gmail.com>
2022-05-15 23:41:51 -0500

    wip on javascript

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Sprite {
     this.velocity = velocity;
     this.height = 150;
   }
+
   draw() {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, 50, this.height);
@@ -22,7 +23,9 @@ class Sprite {
   update() {
     this.draw();
 
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
     } else this.velocity.y += gravity;
@@ -36,7 +39,7 @@ const player = new Sprite({
   },
   velocity: {
     x: 0,
-    y: 10,
+    y: 0,
   },
 });
 
@@ -53,15 +56,43 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+let lastKey;
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+  if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1;
+  }
 }
+
 animate();
 
 window.addEventListener("keydown", (event) => {
-  console.log(event);
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+  }
+  console.log(event.key);
 });
